d1dc83151ca8dbeef6cd0fc98317acd98342e5b6
naskriKam <naskr.kamil@wp.pl>
2023-09-24 20:52:08 +0200

    TS and npm packages
    Setup selectors in typescript, add event listener.
    Install ZOD package to validate request.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import "./style/style.css";
+
+const rollButton = document.querySelector("[data-roll]");
+const adviceContainer = document.querySelector("[data-advice]");
+
+const fetchAdvice = () => {
+  console.log("fetching");
+};
+
+rollButton?.addEventListener("click", fetchAdvice);
